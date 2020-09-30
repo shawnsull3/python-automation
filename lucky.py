91c0d2942325f0136c2922fd7caba33365c7bcb3
Shawn Sullivan <sullivansm3@gmail.com>
2020-09-30 13:52:03 -0600

    Open first 3 links of a google search

--- lucky.py
+++ lucky.py
@@ -1,11 +1,17 @@
-#! python3
+#! /usr/bin/env python3
 # lucky.py - Opens several Google search results for a specific search term
 import requests, sys, webbrowser, bs4
 
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
 print('Googling...') # display text while downloading the Google page
-res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
+res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]), headers=headers)
 res.raise_for_status()
 
+soup = bs4.BeautifulSoup(res.text, 'html.parser')
 
-# TODO: Retrieve top search result links.
-# TODO: Open a browser tab for each result
+links = soup.select('div .g > div > div > a')
+
+numOpen = min(3, len(links))
+for i in range(numOpen):
+    print(links[i]['href'])
+    webbrowser.open(links[i].get('href'))
